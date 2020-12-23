3bedf200514f08e3ac771de76630dd2f334db09b
Rishab Lamba <rishablamba407@gmail.com>
2020-12-23 17:10:45 +0530

    project completed

--- src/UrlContext.js
+++ src/UrlContext.js
@@ -3,9 +3,21 @@ import React, { useState, createContext } from 'react';
 export const UrlContext = createContext();
 
 export const UrlProvider = (props) => {
-    let init = [];
+    let init = [
+        'https://i.imgur.com/jwrT7ig.jpg',
+        'https://i.imgur.com/drw85q3.jpg',
+        'https://i.imgur.com/JAd9Wjf.jpg',
+        'https://i.imgur.com/xQbpaDC.jpg',
+        'https://i.imgur.com/xIFPaOo.jpg',
+        'https://i.imgur.com/faLrovD.jpg',
+        'https://i.imgur.com/jYqN5HX.jpg',
+        'https://i.imgur.com/x8RTgjW.jpg',
+        'https://i.imgur.com/h5s4WnW.jpg',
+    ];
     if (JSON.parse(localStorage.getItem('urls'))) {
         init = JSON.parse(localStorage.getItem('urls'));
+    } else {
+        localStorage.setItem('urls', JSON.stringify(init));
     }
     const [urlList, setUrlList] = useState(init);
 
